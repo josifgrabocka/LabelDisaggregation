0c99826d285d4429bfffe610f5729d77c25c7225
josif <grabocka@informatik.uni-freiburg.de>
2021-10-26 22:54:48 +0200

    early implementation

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ args = parser.parse_args()
 
 config = {'buffer_size': 10000,
           'image_size': (224, 224, 3),
-          'num_epochs': 127,
-          'eta': 0.001,
+          'num_epochs': 255,
+          'eta': 0.0001,
           'batch_size': 100,
           'test_frequency': 1,
           'l2_penalty': 0.0001,
