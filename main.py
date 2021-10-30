366fc1c31c3b05947a55346185604f053ba60d29
josif <grabocka@informatik.uni-freiburg.de>
2021-10-30 21:29:35 +0200

    add ablation of lhw modes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ args = parser.parse_args()
 config = {'buffer_size': 5000,
           'image_size': (224, 224, 3),
           'num_epochs': 100,
-          'eta': 0.0001,
+          'eta': 0.001,
           'batch_size': 100,
           'test_frequency': 1,
           'l2_penalty': 0.00001,
