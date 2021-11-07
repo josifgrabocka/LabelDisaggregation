7c7294e52560fcff4cec03eb413f5dc69067fb17
josif <grabocka@informatik.uni-freiburg.de>
2021-11-07 11:40:28 +0100

    backprop disaggregation through through both true and predicted targets

--- image_classification/LearnHardWay.py
+++ image_classification/LearnHardWay.py
@@ -19,7 +19,8 @@ class LearnHardWay(DefaultOptimizer):
             if units == 0:
                 units = 1
             # add a dense layer with relu activations, unless it is the last layer where the activation is None
-            h = tf.keras.layers.Dense(units=units, activation=None)(h)
+            h = tf.keras.layers.Dense(units=units, activation=None,
+                                      kernel_initializer=tf.keras.initializers.RandomNormal(stddev=0.01))(h)
             if idx < len(config['disaggregation_layers_fracs'])-1:
                 h = tf.keras.layers.Activation('relu')(h)
 
