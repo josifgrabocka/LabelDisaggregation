c7106ce8b8c14bfd94842045607f14a1bd000955
josif <grabocka@informatik.uni-freiburg.de>
2021-11-07 19:38:43 +0100

    backprop disaggregation through through both true and predicted targets

--- image_classification/LearnHardWay.py
+++ image_classification/LearnHardWay.py
@@ -13,19 +13,32 @@ class LearnHardWay(DefaultOptimizer):
         # define the label disaggregation model
         disaggregator_input = tf.keras.Input(self.data_interface.num_classes)
         h = disaggregator_input
-        self.num_disaggregator_layer_units = []
         for idx, frac in enumerate(config['disaggregation_layers_fracs']):
             units=int(frac * self.data_interface.num_classes)
             if units == 0:
                 units = 1
             # add a dense layer with relu activations, unless it is the last layer where the activation is None
-            h = tf.keras.layers.Dense(units=units, activation=None,
-                                      kernel_initializer=tf.keras.initializers.RandomNormal(stddev=1.0))(h)
+            h = tf.keras.layers.Dense(units=units, activation=None)(h)
             if idx < len(config['disaggregation_layers_fracs'])-1:
                 h = tf.keras.layers.Activation('selu')(h)
 
         self.disaggregation_model = tf.keras.Model(inputs=disaggregator_input, outputs=h)
         self.disaggregation_model.summary()
+
+        # define the label disaggregation approximation model
+        disaggregator_input = tf.keras.Input(self.data_interface.num_classes)
+        h = disaggregator_input
+        for idx, frac in enumerate(config['disaggregation_layers_fracs']):
+            units = int(frac * self.data_interface.num_classes)
+            if units == 0:
+                units = 1
+            # add a dense layer with relu activations, unless it is the last layer where the activation is None
+            h = tf.keras.layers.Dense(units=units, activation=None)(h)
+            if idx < len(config['disaggregation_layers_fracs']) - 1:
+                h = tf.keras.layers.Activation('selu')(h)
+
+        self.disaggregation_approx_model = tf.keras.Model(inputs=disaggregator_input, outputs=h)
+        self.disaggregation_approx_model.summary()
 
         #define the additional loss terms metrics
         self.disaggregation_loss_metric = tf.keras.metrics.Mean(name='disaggregation_loss')
@@ -41,6 +54,14 @@ class LearnHardWay(DefaultOptimizer):
         wd = self.l2_penalty * lr_sched(step)
         self.disaggregation_optimizer = tfa.optimizers.AdamW(learning_rate=lr_sched, weight_decay=wd)
 
+        # the cosine decay learning rate scheduler with restarts and the decoupled L2 adam with gradient clipping
+        step = tf.Variable(0, trainable=False)
+        lr_sched = tf.keras.optimizers.schedules.CosineDecayRestarts(initial_learning_rate=config['eta'],
+                                                                     t_mul=1,
+                                                                     first_decay_steps=self.first_decay_steps)
+        wd = self.l2_penalty * lr_sched(step)
+        self.disaggregation_approx_optimizer = tfa.optimizers.AdamW(learning_rate=lr_sched, weight_decay=wd)
+
         # populate the list of models added by child classes of the DefaultOptimizer, such that the saving of these models
         # can happen inside the run method of the parent class
         self.child_classes_models.append(self.disaggregation_model)
@@ -55,9 +76,6 @@ class LearnHardWay(DefaultOptimizer):
     @tf.function
     def train_step(self, x, y):
 
-        z_true = self.disaggregation_model(y, training=False)
-        z_true = tf.one_hot(tf.argmax(z_true, axis=1),
-                            depth=int(self.config['disaggregation_layers_fracs'][-1] * self.data_interface.num_classes))
 
         with tf.GradientTape(persistent=True) as tape:
 
@@ -65,17 +83,23 @@ class LearnHardWay(DefaultOptimizer):
             loss_y = self.cat_loss(y_true=y, y_pred=y_pred)
 
             # define the loss of the disaggregation
+            z_true = self.disaggregation_model(y, training=True)
+            z_pred = self.disaggregation_approx_model(y_pred, training=True)
+            z_true_one_hot = tf.one_hot(tf.argmax(z_true, axis=1), depth=int(self.config['disaggregation_layers_fracs'][-1] * self.data_interface.num_classes))
+            z_pred_one_hot = tf.one_hot(tf.argmax(z_pred, axis=1), depth=int(self.config['disaggregation_layers_fracs'][-1] * self.data_interface.num_classes))
 
-            z_pred = self.disaggregation_model(y_pred, training=True)
-            loss_z = self.disaggregation_loss(y_true=z_true, y_pred=z_pred)
+            loss_z = self.disaggregation_loss(y_true=z_pred_one_hot, y_pred=z_true)
+            loss_z_approx = self.disaggregation_loss(y_true=z_true_one_hot, y_pred=z_pred)
 
             if self.config['lhw_mode'] == 'lhw':
-                loss_prediction_model = loss_y + loss_z
+                loss_prediction_model = loss_y + loss_z_approx
                 loss_disaggregation_model = -tf.tanh(self.disaggregation_gamma*loss_z)
+                loss_disaggregation_approx_model = -tf.tanh(self.disaggregation_gamma * loss_z_approx)
             elif self.config['lhw_mode'] == 'random':
-                loss_prediction_model = loss_y + loss_z
+                loss_prediction_model = loss_y + loss_z_approx
             elif self.config['lhw_mode'] == 'max':
                 loss_disaggregation_model = -tf.tanh(self.disaggregation_gamma*loss_z)
+                loss_disaggregation_approx_model = -tf.tanh(self.disaggregation_gamma * loss_z_approx)
 
         # update the prediction model params
         if self.config['lhw_mode'] == 'lhw' or self.config['lhw_mode'] == 'random':
@@ -89,10 +113,14 @@ class LearnHardWay(DefaultOptimizer):
             disaggregation_gradients = tape.gradient(loss_disaggregation_model, disaggregation_model_weights)
             self.disaggregation_optimizer.apply_gradients(zip(disaggregation_gradients, disaggregation_model_weights))
 
+            disaggregation_approx_model_weights = self.disaggregation_approx_model.trainable_variables
+            disaggregation_approx_gradients = tape.gradient(loss_disaggregation_approx_model, disaggregation_approx_model_weights)
+            self.disaggregation_approx_optimizer.apply_gradients(zip(disaggregation_approx_gradients, disaggregation_approx_model_weights))
+
         # update the metrics
         self.train_loss(loss_y)
         self.train_accuracy(y, y_pred)
-        self.disaggregation_loss_metric(loss_z)
+        self.disaggregation_loss_metric(loss_z_approx)
 
     # def run(self):
     #
